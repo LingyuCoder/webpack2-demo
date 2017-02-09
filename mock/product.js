fac2d23b0f8ce36d373c74fe06a07d22cb7bfcec
天镶 <tianxiang.wly@alibaba-inc.com>
2017-02-09 22:32:34 +0800

    :smile:

--- mock/product.js
+++ mock/product.js
@@ -1,17 +1,35 @@
-/* quote-props: [0] */
+/* eslint quotes: 0 */
+const faker = require('faker');
 module.exports = {
   'GET /api/products': function (req, res) {
     res.json({
-      "success": 1,
+      "success": true,
       "meta": {
         "code": 200
       },
       "data": {
         "products": [{
-          "sku": "11111adfaf",
-          "category": "bbbbb"
-        },
-      ]}
+          "name": faker.commerce.productName(),
+          "price": faker.commerce.price(),
+          "image": faker.image.avatar(),
+          "category": faker.commerce.department()
+        }, {
+          "name": faker.commerce.productName(),
+          "price": faker.commerce.price(),
+          "image": faker.image.avatar(),
+          "category": faker.commerce.department()
+        }, {
+          "name": faker.commerce.productName(),
+          "price": faker.commerce.price(),
+          "image": faker.image.avatar(),
+          "category": faker.commerce.department()
+        }, {
+          "name": faker.commerce.productName(),
+          "price": faker.commerce.price(),
+          "image": faker.image.avatar(),
+          "category": faker.commerce.department()
+        }]
+      }
     });
   },
 };
